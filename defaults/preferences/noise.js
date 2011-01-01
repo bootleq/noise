ca71d473fe5cc1768e082120fddd9f128239681b
bootleq <bootleq@gmail.com>
2011-01-01 20:06:15 +0800

    Version 1.2.1.
    - Add icon.png for dispaly when Noise has been disabled.
    - Bump max version to 4.0.*.

--- defaults/preferences/noise.js
+++ defaults/preferences/noise.js
@@ -1,4 +1,4 @@
 pref("extensions.noise@bootleq.description", "chrome://noise/locale/noise.properties");
-pref("extensions.noise.version", '1.2');
+pref("extensions.noise.version", '1.2.1');
 pref("extensions.noise.enabled", true);
 pref("extensions.noise.base", null);
