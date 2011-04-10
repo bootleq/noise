2a158a7f76de7c7d3b848221e2e5463fd14887c8
bootleq <bootleq@gmail.com>
2011-04-10 15:27:28 +0800

    Add shell script for building xpi.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ADDON="noise"
+ignore="'*.bat' '*.sh' '*.xpi' '*.lnk' '.*' Thumbs.db Desktop.ini build"
+eval rsync -rt . build/ `echo --exclude "${ignore// / --exclude }"`
+cd build/chrome/$ADDON
+zip -r $ADDON.jar .
+mv $ADDON.jar ../
+cd ../../   # build/
+rm -rf chrome/$ADDON
+zip -r $ADDON.xpi .
+cd ..
+mv -f build/$ADDON.xpi .
+rm -rf build
