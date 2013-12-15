12ccf347f0d4513bb34ef54c7492ec9a66aa967f
bootleq <bootleq@gmail.com>
2013-12-15 20:29:24 +0800

    Remove unused dummy functions in NoiseOverlay

--- chrome/noise/content/noise/noise.js
+++ chrome/noise/content/noise/noise.js
@@ -6,39 +6,19 @@ if (!NoiseOverlay) {
 Components.utils.import("resource://noise/noise.jsm");
 
 NoiseOverlay = {
-  player: null,
-  enabled: false,
-
   init: function () {
     Noise.patchWindow(window);
-    this.player = Noise.player;
-    this.prefs = Noise.prefs;
-    this.enabled = Noise.enabled;
     Noise.addEventHandlers(window);
   },
 
   uninit: function () {
     Noise.undoPatchWindow(window);
     Noise.removeEventHandlers(window);
-    this.player = null;
   },
 
   reset: function (newMappings) {
-    this.enabled = this.prefs.getBoolPref("extensions.noise.enabled");
     Noise.removeEventHandlers(window);
     Noise.addEventHandlers(window, newMappings);
-  },
-
-  toggle: function () {
-    Noise.toggle();
-  },
-
-  play: function () {
-    Noise.play.apply(Noise, arguments);
-  },
-
-  log: function (aMessage) {
-    Noise.log(aMessage);
   }
 };
 
