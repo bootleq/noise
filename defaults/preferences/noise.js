977f1308fbd96b23f30972158b2dcb505a01a6ed
bootleq <bootleq@gmail.com>
2013-12-15 22:06:37 +0800

    Version 1.3

--- defaults/preferences/noise.js
+++ defaults/preferences/noise.js
@@ -1,4 +1,4 @@
 pref("extensions.noise@bootleq.description", "chrome://noise/locale/noise.properties");
-pref("extensions.noise.version", '1.3a');
+pref("extensions.noise.version", '1.3');
 pref("extensions.noise.enabled", true);
 pref("extensions.noise.base", null);
