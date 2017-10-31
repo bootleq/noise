46f01095f7bfc8bad9b7734e6763ead05d0c21f6
bootleq <bootleq@gmail.com>
2017-10-31 09:59:09 +0800

    Remove code for legacy version

--- defaults/preferences/noise.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.noise@bootleq.description", "chrome://noise/locale/noise.properties");
-pref("extensions.noise.version", '1.4.3');
-pref("extensions.noise.enabled", true);
-pref("extensions.noise.base", null);
